a002a4a5fe6d4216941d167ef391ab40f3d6e5a7
ssher <ssher250110@yandex.ru>
2024-08-11 22:47:29 +0300

    added admin panel users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Отображение информации о пользователях в админ панели"""
+
+    list_display = [
+        "id",
+        "email",
+        "last_name",
+        "first_name",
+        "middle_name",
+        "phone",
+        "role",
+        "is_active",
+        "last_login",
+    ]
+    search_fields = ["email", "last_name", "first_name", "last_login"]
+    list_filter = ["role", "is_active"]
+    ordering = ["email"]
