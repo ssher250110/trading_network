454c0bb856e072643010c7cc13944b3044053e21
ssher <ssher250110@yandex.ru>
2024-08-11 22:37:55 +0300

    added INSTALLED_APPS and editing LANGUAGE_CODE, TIME_ZONE

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users.apps.UsersConfig",
     "trade.apps.TradeConfig",
+    "rest_framework",
+    "djoser",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -109,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
