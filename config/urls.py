956de3abd1942f605ac184212abfdec9edb15ff3
ssher <ssher250110@yandex.ru>
2024-08-11 23:01:44 +0300

    added path urls users

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("users.urls", namespace="users"))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
