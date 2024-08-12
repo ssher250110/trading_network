5baa8dd2d656d371e55ae4d6b486ff54869c2cf3
ssher <ssher250110@yandex.ru>
2024-08-12 18:55:22 +0300

    editing code

--- trade/permissions.py
+++ trade/permissions.py
@@ -4,6 +4,6 @@ from rest_framework.permissions import BasePermission
 class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if obj.author == request.user:
+        if obj.owner == request.user:
             return True
         return False
