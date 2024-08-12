5e8997c7d692403834ad3d87c7abc4faaa6fb646
ssher <ssher250110@yandex.ru>
2024-08-12 04:06:19 +0300

    added model product

--- trade/models.py
+++ trade/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название продукта", help_text="Укажите название продукта")
+    model = models.CharField(max_length=255, verbose_name="Модель продукта", help_text="Укажите модель продукта")
+    product_release_date = models.DateTimeField(
+        default=timezone.now(),
+        verbose_name="Дата выхода продукта на рынок",
+        help_text="Укажите дату выхода продукта на рынок",
+    )
+    added_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь, добавивший продукт",
+        help_text="Укажите пользователя, добавившего продукт",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
