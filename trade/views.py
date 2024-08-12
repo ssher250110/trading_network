5e9830951a8e5cf092b55f12c9f8781cec7079fb
ssher <ssher250110@yandex.ru>
2024-08-12 18:47:11 +0300

    added permission_classes

--- trade/views.py
+++ trade/views.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView, DestroyAPIView
+from rest_framework.permissions import IsAdminUser
 
-from trade.filters import CityFilter
+from trade.filters import CountryFilter
 from trade.models import LinkNetwork
+from trade.permissions import IsOwner
 from trade.serializers import LinkNetworkSerializer, LinkNetworkDetailSerializer
 
 
@@ -18,7 +20,8 @@ class LinkNetworkListAPIView(ListAPIView):
     queryset = LinkNetwork.objects.all()
     serializer_class = LinkNetworkSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_class = CityFilter
+    filterset_class = CountryFilter
+    permission_classes = [IsAdminUser | IsOwner]
 
     def get_queryset(self):
         return LinkNetwork.objects.filter(owner=self.request.user)
@@ -27,13 +30,16 @@ class LinkNetworkListAPIView(ListAPIView):
 class LinkNetworkRetrieveAPIView(RetrieveAPIView):
     queryset = LinkNetwork.objects.all()
     serializer_class = LinkNetworkDetailSerializer
+    permission_classes = [IsAdminUser | IsOwner]
 
 
 class LinkNetworkUpdateAPIView(UpdateAPIView):
     queryset = LinkNetwork.objects.all()
     serializer_class = LinkNetworkSerializer
+    permission_classes = [IsAdminUser | IsOwner]
 
 
 class LinkNetworkDestroyAPIView(DestroyAPIView):
     queryset = LinkNetwork.objects.all()
     serializer_class = LinkNetworkSerializer
+    permission_classes = [IsAdminUser | IsOwner]
