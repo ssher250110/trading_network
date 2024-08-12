81eb7ef41ff8b7b92d8a1a9d70667b44ceac2a69
ssher <ssher250110@yandex.ru>
2024-08-12 04:21:49 +0300

    added model LinkNetwork

--- trade/models.py
+++ trade/models.py
@@ -26,3 +26,42 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+
+
+class LinkNetwork(models.Model):
+    class Level(models.IntegerChoices):
+        FACTORY = 0
+        RETAIL_NETWORK = 1
+        INDIVIDUAL_BUSINESSMAN = 2
+
+    name = models.CharField(
+        max_length=255, verbose_name="Название звена сети", help_text="Укажите название звена сети"
+    )
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите электронную почту")
+    country = models.CharField(max_length=255, verbose_name="Название страны", help_text="Укажите название страны")
+    city = models.CharField(max_length=255, verbose_name="Название города", help_text="Укажите название города")
+    street = models.CharField(max_length=255, verbose_name="Название улицы", help_text="Укажите название улицы")
+    house_number = models.PositiveSmallIntegerField(verbose_name="Номер дома", help_text="Укажите номер дома")
+    provider = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, **NULLABLE, verbose_name="Поставщик", help_text="Укажите поставщика"
+    )
+    product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, **NULLABLE, verbose_name="Продукт", help_text=" Укажите продукт"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания звена сети")
+    level = models.PositiveSmallIntegerField(
+        choices=Level, verbose_name="Уровень звена сети", help_text="Укажите уровень звена сети"
+    )
+    debt = models.DecimalField(
+        max_digits=9,
+        decimal_places=2,
+        verbose_name="Задолженность перед поставщиком",
+        help_text="Укажите задолженность перед поставщиком",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Звено сети"
+        verbose_name_plural = "Звенья сети"
