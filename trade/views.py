0e7063e9882427eb8893cae53b48d2c2745d0c8f
ssher <ssher250110@yandex.ru>
2024-08-12 18:12:03 +0300

    added APIView model LinkNetwork

--- trade/views.py
+++ trade/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView, DestroyAPIView
 
-# Create your views here.
+from trade.models import LinkNetwork
+from trade.serializers import LinkNetworkSerializer, LinkNetworkDetailSerializer
+
+
+class LinkNetworkCreateAPIView(CreateAPIView):
+    queryset = LinkNetwork.objects.all()
+    serializer_class = LinkNetworkSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LinkNetworkListAPIView(ListAPIView):
+    queryset = LinkNetwork.objects.all()
+    serializer_class = LinkNetworkSerializer
+
+    def get_queryset(self):
+        return LinkNetwork.objects.filter(owner=self.request.user)
+
+
+class LinkNetworkRetrieveAPIView(RetrieveAPIView):
+    queryset = LinkNetwork.objects.all()
+    serializer_class = LinkNetworkDetailSerializer
+
+
+class LinkNetworkUpdateAPIView(UpdateAPIView):
+    queryset = LinkNetwork.objects.all()
+    serializer_class = LinkNetworkSerializer
+
+
+class LinkNetworkDestroyAPIView(DestroyAPIView):
+    queryset = LinkNetwork.objects.all()
+    serializer_class = LinkNetworkSerializer
