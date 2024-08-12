5b3e9e1d00b20b9151445db7880858e9a11e895f
ssher <ssher250110@yandex.ru>
2024-08-12 18:19:37 +0300

    added filter LinkNetworkListAPIView field city

--- trade/views.py
+++ trade/views.py
@@ -1,5 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView, DestroyAPIView
 
+from trade.filters import CityFilter
 from trade.models import LinkNetwork
 from trade.serializers import LinkNetworkSerializer, LinkNetworkDetailSerializer
 
@@ -15,6 +17,8 @@ class LinkNetworkCreateAPIView(CreateAPIView):
 class LinkNetworkListAPIView(ListAPIView):
     queryset = LinkNetwork.objects.all()
     serializer_class = LinkNetworkSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CityFilter
 
     def get_queryset(self):
         return LinkNetwork.objects.filter(owner=self.request.user)
