59d9753078ca4500da6bf2b7f9b395422fbff774
ssher <ssher250110@yandex.ru>
2024-08-12 18:12:27 +0300

    added urls apps

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("users.urls", namespace="users"))
+    path("", include("users.urls", namespace="users")),
+    path("", include("trade.urls", namespace="trade")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
