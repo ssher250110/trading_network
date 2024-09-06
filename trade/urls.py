5b74c77f481da71706b8befb74b7104bf6121ef8
ssher <ssher250110@yandex.ru>
2024-09-06 18:40:22 +0300

    added urls models ContactData and Product

--- trade/urls.py
+++ trade/urls.py
@@ -1,17 +1,28 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
 from trade.apps import TradeConfig
 from trade.views import (
+    ContactDataViewSet,
     LinkNetworkCreateAPIView,
     LinkNetworkListAPIView,
     LinkNetworkRetrieveAPIView,
     LinkNetworkUpdateAPIView,
     LinkNetworkDestroyAPIView,
+    ProductViewSet,
 )
+
+contact_data_router = SimpleRouter()
+contact_data_router.register("contact_data", ContactDataViewSet, basename="contact_data")
+
+product_router = SimpleRouter()
+product_router.register("product", ProductViewSet, basename="product")
 
 app_name = TradeConfig.name
 
 urlpatterns = [
+    path("", include(contact_data_router.urls)),
+    path("", include(product_router.urls)),
     path("link/create/", LinkNetworkCreateAPIView.as_view(), name="link-create"),
     path("link/", LinkNetworkListAPIView.as_view(), name="link-list"),
     path("link/<int:pk>/", LinkNetworkRetrieveAPIView.as_view(), name="link-retrieve"),
